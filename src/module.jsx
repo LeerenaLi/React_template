e84ab0548726aff0c6b2c0a8d0703e43bd74044d
Leerena <leerena7@gmail.com>
2023-07-28 11:28:12 +0300

    add template

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import arrow from './img/arrow.jpg';
+
+const Module = () => (
+    <>
+        <h1>Hello, React!</h1>
+        <img src={arrow} />
+    </>
+);
+
+export default Module;
